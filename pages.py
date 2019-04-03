b0a6837f151dc08193e47618c1fbb171d9b0c7aa
Sasha1152 <saxin@ukr.net>
2019-04-03 15:47:09 +0300

    create the classs 'GeneralPage'

--- pages.py
+++ pages.py
@@ -1,17 +1,13 @@
-class GooglePage:
+class GeneralPage:
+    def __init__(self, browser):
+        self.browser = browser
+
+
+class GooglePage(GeneralPage):
     search_field_name = 'q'
     text_to_search = 'pizza lviv'
     start_page_url = 'http://www.google.com'
 
-    def __init__(self, browser):
-        self.browser = browser
 
-    # def search_text(self):
-    #     self.browser.find_element_by_css_selector(search_field)
-
-
-class PizzaLvivPage:
+class PizzaLvivPage(GeneralPage):
     start_page_pizza_title = 'Доставка піци Львів'
-
-    def __init__(self, browser):
-        self.browser = browser
