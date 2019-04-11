90bda69b9af2f570ad967d3679d19bbe9a5f93d0
Sasha1152 <saxin@ukr.net>
2019-04-11 16:06:03 +0300

    refactor code

--- tools/common_tools.py
+++ tools/common_tools.py
@@ -2,16 +2,29 @@ from selenium.webdriver.common.keys import Keys
 import os
 from importlib import import_module
 
+def get_class_names():
+    classes = {}
+    filenames = os.listdir('../page_objects')
+    for f in filenames:
+        name = f.split('.')[0]
+        class_name = ''.join(x.capitalize() or '_' for x in name.split('_'))
+        if class_name == 'GeneralPage' or class_name == '__Pycache__':
+            continue
+        classes[name] = class_name
+    return classes
 
-filenames = os.listdir('../page_objects')
-for f in filenames:
-    name = f.split('.')[0]
-    class_name = ''.join(x.capitalize() or '_' for x in name.split('_'))
-    if class_name == 'GeneralPage' or class_name == '__Pycache__':
-        continue
+classes = get_class_names()
+for name, class_name in classes.items():
     path = f'page_objects.{name}'
     mod = import_module(path)
     globals()[class_name] = eval('mod.' + class_name)
+
+
+def set_dynamic_instances(context):
+    for name, class_name in classes.items():
+        class_obj = globals()[class_name](context.browser)
+        setattr(context, name, class_obj)
+    return context
 
 
 def type_in_textfield(context, text_to_type):
@@ -19,15 +32,3 @@ def type_in_textfield(context, text_to_type):
     element.click()
     element.send_keys(text_to_type)
     element.send_keys(Keys.RETURN)
-
-
-def set_dynamic_instances(context):
-    filenames = os.listdir('../page_objects')
-    for f in filenames:
-        name = f.split('.')[0]
-        class_name = ''.join(x.capitalize() or '_' for x in name.split('_'))
-        if class_name == 'GeneralPage' or class_name == '__Pycache__':
-            continue
-        class_obj = globals()[class_name](context.browser)
-        setattr(context, name, class_obj)
-    return context
